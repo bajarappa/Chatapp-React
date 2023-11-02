1dcc86266dc4c9c08c09ce3f6ff9f410c4ef53d2
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-11-02 20:17:31 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,30 @@ import ChatPage from "./pages/ChatPage";
 import { store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Conversation from "./components/leftPanel/Conversation";
 
 let persistor = persistStore(store);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ChatPage />,
+    children: [
+      {
+        path: "conversation/:id",
+        element: <Conversation />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <ChatPage />
+          <RouterProvider router={router} />
         </PersistGate>
       </Provider>
     </>
